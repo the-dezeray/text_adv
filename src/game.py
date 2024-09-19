1eacf2da43e63b280630523ac018f6729fd152f3
the-dezeray <chingwaru.desiree@gmail.com>
2024-09-19 10:05:57 +0200

    feat : working option

--- src/game.py
+++ src/game.py
@@ -1,7 +1,11 @@
 from rich.panel import Panel
 from rich.table import Table
 from rich.padding import Padding
-from rich.layout import layout
+from rich.layout import Layout
+from rich.console import Console
+from file_handler import load_yaml_file
+from options import Option
+from main_layout import character_selection_layout,character_preview_layout,gameplay_layout
 class Game():
     def __init__(self,interface = None) -> None:
         self.running = True
@@ -37,9 +41,7 @@ class Game():
         self.in_game = True
         self.interface   = gameplay_layout()
         self.game_loop()
-        
-    def exit_game(self):pass
-    def refresh(self):
+    def option_table(self):
         self.table = Table(expand=True,show_edge=False,show_header=False)
         self.table.add_column()
                 
@@ -51,8 +53,20 @@ class Game():
                     self.options[i].preview()
                 style = "bold green"
                 x += 5
-            self.table.add_row(Padding(Panel(option.text,border_style=style),pad =(0,0,0,x))) 
-        
+            #self.table.add_row(Padding(Panel(option.text,border_style=style),pad =(0,0,0,x))) 
+            self.table.add_row(self.menu_button(text = option.text,style = style,x=x))
+    def menu_button(self,text,style,x):
+        instance : Padding = Padding (
+                Panel(
+                    text,
+                    border_style = style
+                    ),
+                pad = (0,0,0,x)
+        )
+        return instance
+    def exit_game(self):pass
+    def refresh(self):
+        self.option_table()
         self.interface["options"].update(self.table)
     def save_key(self,key):
         """handles keyboard input"""
@@ -91,11 +105,11 @@ class Game():
             table.add_column()
             table.add_row(Padding(Panel(current_chapter["text"]),pad =(0,20,0,0)))
             #interface.display(current_chapter["text"])
-
+            self.options = []
             for index,choice in enumerate(current_chapter["choices"]):
                 
                 table.add_row(Padding(Panel(f"{index}.{choice['text']}"),pad=(0,100,0,0)))
-
+                self.options.append(choice['function'])
             
             self.interface["preview"].update(table)
             self.love.update(self.interface)
--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import time
 from main_layout import character_preview_layout,gameplay_layout,character_selection_layout
 from pynput.keyboard  import Listener
 from game import Game
-
-
+from rich.console import Console
+from rich.live import Live
 console = Console()
 
 def main():
@@ -14,9 +14,7 @@ def main():
     
     core = Game()
 
-    #listens for keyboard key press
     with Listener(on_press= core.save_key) as core.key_listener:
-        #Renders an auto-updating terminal
         with Live(core.interface, refresh_per_second=10) as core.love:  # update 10  times a second to feel fluid
             
             while core.running: #if program has not been terminated
