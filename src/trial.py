6bec84584f6c215a1c4f40f2d0d5c886141f05ea
the-dezeray <chingwaru.desiree@gmail.com>
2025-01-02 23:49:23 +0200

    docs: empty

--- src/trial.py
+++ src/trial.py
@@ -1,4 +1,21 @@
+# new word
 import time
 
+input("Press Enter to continue...")
 
-a = time.ctime()
+start_time = time.time()
+last_time = time.time()
+lap_num = 1
+
+
+try:
+    while True:
+        input()
+        lap_time = round(time.time() - last_time,2 )
+        total_time = round (time.time() - start_time,2)
+        print(f"lap number:{lap_num}  {total_time} {lap_time}")
+        lap_num +=1
+        last_time = time.time()
+except KeyboardInterrupt:
+    print("program ended")
+        
