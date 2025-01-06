8b4d42812b4c6523b963a9410906ed3cfa0a08f9
the-dezeray <chingwaru.desiree@gmail.com>
2025-01-06 23:18:03 +0200

    refac: clean up

--- src/are.py
+++
@@ -1,6 +0,0 @@
-from rich.console import Console
-from rich.spinner import Spinner
-console = Console()
-console.input("What is [i]your[/i] [bold red]name[/]? :smiley: ")
-console.rule("[bold red]Chapter 1")
-spinner = Spinner("dots", text="Loading...")
