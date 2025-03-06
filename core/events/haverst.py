ded217bfd6999dc98c1bcb423cb7698777f29476
the-dezeray <chingwaru.desiree@gmail.com>
2025-03-06 23:37:03 +0200

    feat: add type hint for core parameter in harvest function

--- core/events/haverst.py
+++ core/events/haverst.py
@@ -1,2 +1,7 @@
-def harvest(core= None):
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    # Import only for type checking (not at runtime)
+    from core.core import Core
+
+def harvest(core :"Core"= None):
     pass
