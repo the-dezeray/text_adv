6accbc2a2d23a101825f215bf926a34aeb0736b1
the-dezeray <chingwaru.desiree@gmail.com>
2025-06-30 23:30:47 +0200

    feat: modify attempt steal

--- core/events/attempt_steal.py
+++ core/events/attempt_steal.py
@@ -13,10 +13,8 @@ if TYPE_CHECKING:
 
 def attempt_steal(core: "Core" ,item="",stealth_check  =0,on_success = None,on_fail = None):
     core.console.clear_display()
-    text = "you have obtained"
+    text = "[red]"+("||" * 10)+"[/red]"
     core.console.print(
         ui_text_panel(text=text)
     )
-
-    core.console.print(Option(text="pick up", func=lambda: core.goto_next() ))
-    core.console.print(Option(text="leave", func=lambda: core.goto_next(),))
+    core.goto_next()
